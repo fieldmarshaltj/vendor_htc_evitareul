11293d07d3937005c88bb3ed689f1aac3b3ae19b
Rob <Lloir@kanger.me>
2014-02-13 16:25:20 -0500

    THIS POINT IS STABLE

--- evitareul-vendor-blobs.mk
+++ evitareul-vendor-blobs.mk
@@ -87,12 +87,7 @@ vendor/htc/evitareul/proprietary/lib/egl/libGLESv1_CM_tegra.so:system/lib/egl/li
 vendor/htc/evitareul/proprietary/lib/egl/libGLESv2_perfhud.so:system/lib/egl/libGLESv2_perfhud.so \
 vendor/htc/evitareul/proprietary/lib/egl/libGLESv2_tegra_impl.so:system/lib/egl/libGLESv2_tegra_impl.so \
 vendor/htc/evitareul/proprietary/lib/egl/libGLESv2_tegra.so:system/lib/egl/libGLESv2_tegra.so \
-vendor/htc/evitareul/proprietary/lib/hw/audio.a2dp.default.so:system/lib/hw/audio.a2dp.default.so \
-vendor/htc/evitareul/proprietary/lib/hw/audio.primary.default.so:system/lib/hw/audio.primary.default.so \
 vendor/htc/evitareul/proprietary/lib/hw/audio.primary.tegra.so:system/lib/hw/audio.primary.tegra.so \
-vendor/htc/evitareul/proprietary/lib/hw/audio_policy.default.so:system/lib/hw/audio_policy.default.so \
-vendor/htc/evitareul/proprietary/lib/hw/audio_policy.tegra.so:system/lib/hw/audio_policy.tegra.so \
-vendor/htc/evitareul/proprietary/lib/hw/audio.usb.default.so:system/lib/hw/audio.usb.default.so \
 vendor/htc/evitareul/proprietary/lib/hw/camera.tegra.so:system/lib/hw/vendor-camera.tegra.so \
 vendor/htc/evitareul/proprietary/lib/hw/gps.default.so:system/lib/hw/gps.default.so \
 vendor/htc/evitareul/proprietary/lib/hw/gps.tegra.so:system/lib/hw/gps.tegra.so \
